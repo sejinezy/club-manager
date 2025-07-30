8e0d827ec20cc2d6088d8a39c5bfd7ac9fe33f5e
sejin <psj8880@naver.com>
2025-07-30 11:40:00 +0900

    project init

---
+++ src/main/java/com/sejin/clubmanager/ClubManagerApplication.java
@@ -0,0 +1,13 @@
+package com.sejin.clubmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sejin/clubmanager/ClubManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sejin.clubmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
