995d19292b22a9bfca9a6e24b95456be3ec5f5f3
sejin <psj8880@naver.com>
2025-07-30 13:09:23 +0900

    feat(ClubRepository): ClubRepository 생성
    동아리 Repository 생성

---
+++ src/main/java/com/sejin/clubmanager/club/db/ClubRepository.java
@@ -0,0 +1,6 @@
+package com.sejin.clubmanager.club.db;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClubRepository extends JpaRepository<ClubEntity,Long> {
+}
