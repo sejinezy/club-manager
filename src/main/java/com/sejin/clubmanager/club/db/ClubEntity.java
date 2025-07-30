afd1ec175e0b93827e76717f47b4250d36b7cdc0
sejin <psj8880@naver.com>
2025-07-30 13:07:28 +0900

    feat(ClubEntity): ClubEntity 생성 및 JPA 매핑 설정
    동아리 엔티티 생성

---
+++ src/main/java/com/sejin/clubmanager/club/db/ClubEntity.java
@@ -0,0 +1,56 @@
+package com.sejin.clubmanager.club.db;
+
+import com.sejin.clubmanager.member.db.MemberEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Builder.Default;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@Entity(name="club")
+@EntityListeners(AuditingEntityListener.class)
+public class ClubEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "club", fetch = FetchType.LAZY)
+    @Builder.Default
+    private List<MemberEntity> memberEntities = new ArrayList<>();
+
+}
