9c5a14385a6b2fa0d807af92122e8cae7c69a6e5
sejin <psj8880@naver.com>
2025-07-30 14:12:13 +0900

    feat(CrudAbstractService): CrudAbstractService<T,E> 공통 서비스 로직 구현
    CRUD 서비스 추상화 구현

---
+++ src/main/java/com/sejin/clubmanager/crud/CrudAbstractService.java
@@ -0,0 +1,76 @@
+package com.sejin.clubmanager.crud;
+
+import com.sejin.clubmanager.common.Api;
+import com.sejin.clubmanager.common.Pagination;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class CrudAbstractService<DTO, ENTITY> implements CrudInterface<DTO> {
+
+    @Autowired(required = false)
+    private Converter<DTO, ENTITY> converter;
+
+    @Autowired
+    private JpaRepository<ENTITY, Long> jpaRepository;
+
+    @Override
+    public DTO create(DTO dto) {
+        ENTITY entity = converter.toEntity(dto);
+        jpaRepository.save(entity);
+        return converter.toDto(entity);
+    }
+
+    @Override
+    public Optional<DTO> read(Long id) {
+        Optional<ENTITY> optionalEntity = jpaRepository.findById(id);
+
+        DTO dto = optionalEntity.map(
+                it -> converter.toDto(it)
+        ).orElse(null);
+
+        return Optional.ofNullable(dto);
+    }
+
+    @Override
+    public DTO update(DTO dto) {
+        ENTITY entity = converter.toEntity(dto);
+        jpaRepository.save(entity);
+        return converter.toDto(entity);
+    }
+
+    @Override
+    public void delete(Long id) {
+        jpaRepository.deleteById(id);
+
+    }
+
+    @Override
+    public Api<List<DTO>> list(Pageable pageable) {
+        Page<ENTITY> list = jpaRepository.findAll(pageable);
+
+        Pagination pagination = Pagination.builder()
+                .page(list.getNumber())
+                .size(list.getSize())
+                .currentElements(list.getNumberOfElements())
+                .totalElements(list.getTotalElements())
+                .totalPage(list.getTotalPages())
+                .build();
+
+        List<DTO> dtoList = list.stream()
+                .map(it -> {
+                    return converter.toDto(it);
+                })
+                .toList();
+
+        Api<List<DTO>> response = Api.<List<DTO>>builder()
+                .body(dtoList)
+                .pagination(pagination)
+                .build();
+
+        return response;
+    }
+}
