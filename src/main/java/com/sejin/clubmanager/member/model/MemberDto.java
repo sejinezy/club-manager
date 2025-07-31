32f95ee234e13cfbb2d45452f044024cf17a22ed
sejin <psj8880@naver.com>
2025-07-31 10:38:54 +0900

    feat(MemberDto): MemberDto 구현

---
+++ src/main/java/com/sejin/clubmanager/member/model/MemberDto.java
@@ -0,0 +1,34 @@
+package com.sejin.clubmanager.member.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.sejin.clubmanager.crud.Identifiable;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MemberDto implements Identifiable<Long> {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private Long clubId;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+}
