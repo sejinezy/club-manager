337e73ac33cbe5dfd15d86b05618462f8b1e7708
sejin <psj8880@naver.com>
2025-08-12 12:50:11 +0900

    feat(MemberRequest): Member 관련 Request DTO 구현
    - 입력 데이터 유효성 검증

---
+++ src/main/java/com/sejin/clubmanager/member/model/MemberRequest.java
@@ -0,0 +1,33 @@
+package com.sejin.clubmanager.member.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MemberRequest {
+
+    @NotBlank
+    private String name;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotNull
+    private Long clubId;
+}
