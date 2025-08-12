f5fb1c55465637fa11abdf6295b122cd82ae22be
sejin <psj8880@naver.com>
2025-08-12 12:53:42 +0900

    refactor: Request / Response DTO 구분 적용

--- src/main/java/com/sejin/clubmanager/member/controller/MemberApiController.java
+++ src/main/java/com/sejin/clubmanager/member/controller/MemberApiController.java
@@ -2,11 +2,12 @@ package com.sejin.clubmanager.member.controller;
 
 import com.sejin.clubmanager.crud.CrudApiAbstractApiController;
 import com.sejin.clubmanager.member.db.MemberEntity;
-import com.sejin.clubmanager.member.model.MemberDto;
+import com.sejin.clubmanager.member.model.MemberRequest;
+import com.sejin.clubmanager.member.model.MemberResponse;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/member")
-public class MemberApiController extends CrudApiAbstractApiController<MemberDto, MemberEntity> {
+public class MemberApiController extends CrudApiAbstractApiController<MemberRequest, MemberResponse, MemberEntity> {
 }
--- src/main/java/com/sejin/clubmanager/member/service/MemberService.java
+++ src/main/java/com/sejin/clubmanager/member/service/MemberService.java
@@ -2,9 +2,10 @@ package com.sejin.clubmanager.member.service;
 
 import com.sejin.clubmanager.crud.CrudAbstractService;
 import com.sejin.clubmanager.member.db.MemberEntity;
-import com.sejin.clubmanager.member.model.MemberDto;
+import com.sejin.clubmanager.member.model.MemberRequest;
+import com.sejin.clubmanager.member.model.MemberResponse;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MemberService extends CrudAbstractService<MemberDto, MemberEntity> {
+public class MemberService extends CrudAbstractService<MemberRequest, MemberResponse, MemberEntity> {
 }
