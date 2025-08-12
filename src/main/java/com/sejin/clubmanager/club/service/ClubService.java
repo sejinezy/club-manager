a07dffe5ba9eb1e11e85760f79c8e40c89799ab6
sejin <psj8880@naver.com>
2025-08-12 12:48:59 +0900

    refactor: Request/ Response DTO 구분 적용

--- src/main/java/com/sejin/clubmanager/club/service/ClubService.java
+++ src/main/java/com/sejin/clubmanager/club/service/ClubService.java
@@ -1,14 +1,15 @@
 package com.sejin.clubmanager.club.service;
 
 import com.sejin.clubmanager.club.db.ClubEntity;
-import com.sejin.clubmanager.club.model.ClubDto;
+import com.sejin.clubmanager.club.model.ClubRequest;
+import com.sejin.clubmanager.club.model.ClubResponse;
 import com.sejin.clubmanager.crud.CrudAbstractService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class ClubService extends CrudAbstractService<ClubDto, ClubEntity> {
+public class ClubService extends CrudAbstractService<ClubRequest, ClubResponse, ClubEntity> {
 
 
 }
