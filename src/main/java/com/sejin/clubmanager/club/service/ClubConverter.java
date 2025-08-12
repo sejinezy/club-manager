8750c77939503e889deda137c690ab77208c8e91
sejin <psj8880@naver.com>
2025-08-12 12:47:50 +0900

    refactor(ClubConverter): ClubRequest , ClubResponse, ClubEntity 간의 컨버터 구현
    - CREATE : ClubRequest -> ClubEntity
    - UPDATE : ClubRequest -> 기존
    ClubEntity 필드 갱신
    - READ,WRITE  응답 :  ClubEntity -> ClubResponse

--- src/main/java/com/sejin/clubmanager/club/service/ClubConverter.java
+++ src/main/java/com/sejin/clubmanager/club/service/ClubConverter.java
@@ -1,10 +1,10 @@
 package com.sejin.clubmanager.club.service;
 
 import com.sejin.clubmanager.club.db.ClubEntity;
-import com.sejin.clubmanager.club.model.ClubDto;
+import com.sejin.clubmanager.club.model.ClubRequest;
+import com.sejin.clubmanager.club.model.ClubResponse;
 import com.sejin.clubmanager.crud.Converter;
-import com.sejin.clubmanager.member.db.MemberEntity;
-import com.sejin.clubmanager.member.model.MemberDto;
+import com.sejin.clubmanager.member.model.MemberResponse;
 import com.sejin.clubmanager.member.service.MemberConverter;
 import java.util.List;
 import lombok.AllArgsConstructor;
@@ -12,51 +12,38 @@ import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
-public class ClubConverter implements Converter<ClubDto, ClubEntity> {
+public class ClubConverter implements Converter<ClubRequest, ClubResponse, ClubEntity> {
 
     private final MemberConverter memberConverter;
 
     @Override
-    public ClubDto toDto(ClubEntity clubEntity) {
-
-        List<MemberDto> memberDtos = clubEntity.getMemberEntities().stream()
-                .map(memberConverter::toDto).toList();
-
-        return ClubDto.builder()
-                .id(clubEntity.getId())
-                .name(clubEntity.getName())
-                .description(clubEntity.getDescription())
-                .createdAt(clubEntity.getCreatedAt())
-                .updatedAt(clubEntity.getUpdatedAt())
-                .memberEntities(memberDtos)
-                .build();
-
-
-    }
-
-    @Override
-    public ClubEntity toEntity(ClubDto clubDto) {
-
-        List<MemberEntity> memberEntities = clubDto.getMemberEntities().stream()
-                .map(memberConverter::toEntity).toList();
-
+    public ClubEntity toEntity(ClubRequest req) {
         return ClubEntity.builder()
-                .id(clubDto.getId())
-                .name(clubDto.getName())
-                .description(clubDto.getDescription())
-                .memberEntities(memberEntities)
+                .name(req.getName())
+                .description(req.getDescription())
                 .build();
     }
 
     @Override
-    public void updateEntityFromDto(ClubDto clubDto, ClubEntity clubEntity) {
-        List<MemberEntity> memberEntities = clubDto.getMemberEntities().stream()
-                .map(memberConverter::toEntity).toList();
+    public void updateEntityFromRequest(ClubRequest req, ClubEntity entity) {
+        entity.setName(req.getName());
+        entity.setDescription(req.getDescription());
+    }
 
-        clubEntity.setName(clubDto.getName());
-        clubEntity.setDescription(clubDto.getDescription());
-        clubEntity.setMemberEntities(memberEntities);
+    @Override
+    public ClubResponse toResponse(ClubEntity entity) {
 
+        List<MemberResponse> members = entity.getMemberEntities().stream()
+                .map(memberConverter::toResponse)
+                .toList();
 
+        return ClubResponse.builder()
+                .id(entity.getId())
+                .name(entity.getName())
+                .description(entity.getDescription())
+                .createdAt(entity.getCreatedAt())
+                .updatedAt(entity.getUpdatedAt())
+                .members(members)
+                .build();
     }
 }
