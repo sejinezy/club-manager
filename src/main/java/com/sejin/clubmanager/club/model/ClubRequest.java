cc74f31e7ec41196d2a83a15c79e4e2c8f5c49e9
sejin <psj8880@naver.com>
2025-08-12 12:43:12 +0900

    feat(ClubRequest): 요청 객체 구현
    - Club과 관련된 RESTAPI 요청을 받을 때 필요한 ClubRequest 객체 구현

---
+++ src/main/java/com/sejin/clubmanager/club/model/ClubRequest.java
@@ -0,0 +1,29 @@
+package com.sejin.clubmanager.club.model;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class ClubRequest {
+
+    @NotBlank
+    private String name;
+
+    @Size(max = 2000)
+    private String description;
+
+}
