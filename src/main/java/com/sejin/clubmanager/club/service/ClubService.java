89b8c49d18e2ab79689cfd60eb5fc5cc14da1cda
sejin <psj8880@naver.com>
2025-08-13 13:48:31 +0900

    feat(ClubService): preDelete 구현
    - 삭제하고자 하는 동아리에 활성 회원이 있으면 동아리 삭제 차단
    - IllegalStateException 발생

--- src/main/java/com/sejin/clubmanager/club/service/ClubService.java
+++ src/main/java/com/sejin/clubmanager/club/service/ClubService.java
@@ -4,6 +4,7 @@ import com.sejin.clubmanager.club.db.ClubEntity;
 import com.sejin.clubmanager.club.model.ClubRequest;
 import com.sejin.clubmanager.club.model.ClubResponse;
 import com.sejin.clubmanager.crud.CrudAbstractService;
+import com.sejin.clubmanager.member.db.MemberRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,5 +12,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class ClubService extends CrudAbstractService<ClubRequest, ClubResponse, ClubEntity> {
 
+    private final MemberRepository memberRepository;
+
+    @Override
+    protected void preDelete(Long id) {
+        if (memberRepository.existsByClub_IdAndDeletedFalse(id)) {
+            throw new IllegalStateException("해당 동아리에 회원이 있어 삭제할 수 없습니다.");
+        }
+    }
 
 }
