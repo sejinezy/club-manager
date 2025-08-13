49e3eddd968be7faeabcc21a7eea78fb02f40af8
sejin <psj8880@naver.com>
2025-08-13 21:12:56 +0900

    refactor(CrudApiAbstractApiController): Api 응답 스펙으로 감싸서 응답 반환
    컨트롤러가 서비스 결과를 받아 Api<>로 감싸는 조립 담당

--- src/main/java/com/sejin/clubmanager/crud/CrudApiAbstractApiController.java
+++ src/main/java/com/sejin/clubmanager/crud/CrudApiAbstractApiController.java
@@ -1,11 +1,14 @@
 package com.sejin.clubmanager.crud;
 
 import com.sejin.clubmanager.common.Api;
+import com.sejin.clubmanager.common.Pagination;
 import jakarta.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,28 +19,46 @@ import org.springframework.web.bind.annotation.RequestBody;
 public abstract class CrudApiAbstractApiController<REQ,RES extends Identifiable<Long>, ENTITY> implements CrudInterface<REQ,RES> {
 
     @Autowired(required = false)
-    private CrudAbstractService<REQ, RES, ENTITY> crudAbstractService;
+    private CrudServicePort<REQ,RES> crudAbstractService;
 
 
     @PostMapping
     @Override
-    public RES create(
+    public Api<RES> create(
             @Valid @RequestBody REQ req) {
-        return crudAbstractService.create(req);
+        RES res = crudAbstractService.create(req);
+        return Api.<RES>builder()
+                .resultCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage(HttpStatus.OK.name())
+                .body(res)
+                .build();
     }
 
     @GetMapping("/id/{id}")
     @Override
-    public Optional<RES> read(
+    public Api<Optional<RES>> read(
             @PathVariable Long id) {
-        return crudAbstractService.read(id);
+        Optional<RES> res = crudAbstractService.read(id);
+
+        return Api.<Optional<RES>>builder()
+                .resultCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage(HttpStatus.OK.name())
+                .body(res)
+                .build();
+
     }
 
     @PutMapping("/id/{id}")
     @Override
-    public RES update(
+    public Api<RES> update(
             @PathVariable Long id, @Valid @RequestBody REQ req) {
-        return crudAbstractService.update(id,req);
+        RES res = crudAbstractService.update(id, req);
+        return Api.<RES>builder()
+                .resultCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage(HttpStatus.OK.name())
+                .body(res)
+                .build();
+
     }
 
     @DeleteMapping("/id/{id}")
@@ -50,6 +71,23 @@ public abstract class CrudApiAbstractApiController<REQ,RES extends Identifiable<
     @GetMapping("/all")
     @Override
     public Api<List<RES>> list(Pageable pageable) {
-        return crudAbstractService.list(pageable);
+
+        Page<RES> page = crudAbstractService.list(pageable);
+
+        Pagination pagination = Pagination.builder()
+                .page(page.getNumber())
+                .size(page.getSize())
+                .currentElements(page.getNumberOfElements())
+                .totalElements(page.getTotalElements())
+                .totalPage(page.getTotalPages())
+                .build();
+
+        return Api.<List<RES>>builder()
+                .resultCode(String.valueOf(HttpStatus.OK.value()))
+                .resultMessage(HttpStatus.OK.name())
+                .body(page.getContent())
+                .pagination(pagination)
+                .build();
+
     }
 }
