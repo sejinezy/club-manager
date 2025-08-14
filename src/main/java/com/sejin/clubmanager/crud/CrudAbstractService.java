6defcfb9faaa1c5095b451aaad60a7703ca10eb0
sejin <psj8880@naver.com>
2025-08-14 15:53:39 +0900

    refactor(CrudAbstractService): 의존성 주입 방식 변경
    - 기존 방식은 주입 실패시 null이 들어가, 실행 중 널포인터가 발생하는 문제가 있음
    -
    부모(CrudAbstractService)는 추상 메서드로 의존성을 요구하고, 자식에서 구체 타입을 리턴하도록 수정

--- src/main/java/com/sejin/clubmanager/crud/CrudAbstractService.java
+++ src/main/java/com/sejin/clubmanager/crud/CrudAbstractService.java
@@ -7,55 +7,56 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
+
+@Transactional(readOnly = true)
 public abstract class CrudAbstractService<REQ,RES extends Identifiable<Long>, ENTITY> implements CrudServicePort<REQ,RES> {
 
-    @Autowired(required = false)
-    private Converter<REQ, RES, ENTITY> converter;
 
-    @Autowired
-    private JpaRepository<ENTITY, Long> jpaRepository;
+    protected abstract Converter<REQ, RES, ENTITY> converter();
+    protected abstract JpaRepository<ENTITY, Long> jpaRepository();
 
+    @Transactional
     @Override
     public RES create(REQ req) {
-        ENTITY entity = converter.toEntity(req);
-        jpaRepository.save(entity);
-        return converter.toResponse(entity);
+        ENTITY entity = converter().toEntity(req);
+        jpaRepository().save(entity);
+        return converter().toResponse(entity);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public RES read(Long id) {
-        return jpaRepository.findById(id)
-                .map(converter::toResponse)
+        return jpaRepository().findById(id)
+                .map(converter()::toResponse)
                 .orElseThrow(() -> new EntityNotFoundException("해당 ID의 데이터가 없습니다. ID=" + id));
     }
 
+    @Transactional
     @Override
     public RES update(Long id, REQ req) {
-        ENTITY entity = jpaRepository.findById(id)
+        ENTITY entity = jpaRepository().findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 ID의 데이터가 없습니다."));
 
-        converter.updateEntityFromRequest(req, entity);
+        converter().updateEntityFromRequest(req, entity);
 
-        jpaRepository.flush();
+        jpaRepository().flush();
 
-        return converter.toResponse(entity);
+        return converter().toResponse(entity);
 
     }
 
+    @Transactional
     @Override
     public void delete(Long id) {
         preDelete(id);
-        jpaRepository.deleteById(id);
+        jpaRepository().deleteById(id);
         postDelete(id);
     }
 
     @Override
-    @Transactional(readOnly = true)
     public Page<RES> list(Pageable pageable) {
 
-        return jpaRepository.findAll(pageable)
-                .map(converter::toResponse);
+        return jpaRepository().findAll(pageable)
+                .map(converter()::toResponse);
     }
 
     protected void preDelete(Long id) {}
